a6b8fc90129be2ae78f5d38a537dc64353124439
Vasil Dimov <vd@FreeBSD.org>
2020-10-22 20:34:31 +0200

    net: don't extra bind for Tor if binds are restricted
    The semantic of `-bind` is to restrict the binding only to some address.
    If not specified, then the user does not care and we bind to `0.0.0.0`.
    If specified then we should honor the restriction and bind only to the
    specified address.
    
    Before this change, if no `-bind` is given then we would bind to
    `0.0.0.0` and to `127.0.0.1:8334` (incoming Tor) which is ok - the user
    does not care to restrict the binding. However:
    
    * If only `-bind=addr` is given (without `-bind=...=onion`) then we
      would bind to `addr` _and_ to `127.0.0.1:8334` in addition which may
      be unexpected assuming the perception of `-bind=addr` is
      "bind _only_ to `addr`".
    
    * If only `-bind=addr=onion` is given (without ordinary `-bind=`)
      then we would bind to `addr` _and_ to `0.0.0.0` in addition.
    
    Change the above two cases to not do the additional bind. If `-bind`
    is given then only bind to the specified address:
    
    * If only `-bind=addr` is given (without `-bind=...=onion`) then bind to
      `addr` (only). If we are creating tor hidden service then use `addr`
      as target. Same behavior as before
      https://github.com/bitcoin/bitcoin/pull/19991.
    
    * If only `-bind=addr=onion` is given (without ordinary `-bind=`)
      then bind to `addr` (only) and consider incoming connections as Tor
      and do not advertise `addr`. I.e. a Tor-only node.

---
+++ test/functional/feature_bind_extra.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-2020 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+Test starting bitcoind with -bind and/or -bind=...=onion and confirm
+that bind happens on the expected ports.
+"""
+
+import sys
+
+from test_framework.netutil import (
+    addr_to_hex,
+    get_bind_addrs,
+)
+from test_framework.test_framework import (
+    BitcoinTestFramework,
+    SkipTest,
+)
+from test_framework.util import (
+    PORT_MIN,
+    PORT_RANGE,
+    assert_equal,
+    p2p_port,
+    rpc_port,
+)
+
+# From chainparamsbase.cpp:CreateBaseChainParams().
+REGTEST_TOR_TARGET_PORT = 18445
+
+class BindExtraTest(BitcoinTestFramework):
+    def set_test_params(self):
+        # Avoid any -bind= on the command line. Force the framework to avoid
+        # adding -bind=127.0.0.1.
+        self.setup_clean_chain = True
+        self.bind_to_localhost_only = False
+        self.num_nodes = 4
+
+    def setup_network(self):
+        # Override setup_network() because we want to put the result of
+        # p2p_port() in self.extra_args[], before the nodes are started.
+        # p2p_port() is not usable in set_test_params() because PortSeed.n is
+        # not set at that time.
+
+        # Due to OS-specific network stats queries, we only run on Linux.
+        self.log.info("Checking for Linux")
+        if not sys.platform.startswith('linux'):
+            raise SkipTest("This test can only be run on Linux.")
+
+        any_ipv4 = addr_to_hex('0.0.0.0')
+        loopback_ipv4 = addr_to_hex('127.0.0.1')
+
+        # Start custom ports after p2p and rpc ports.
+        port = PORT_MIN + 2 * PORT_RANGE
+
+        # Array of tuples [command line arguments, expected bind addresses].
+        self.expected = []
+
+        # Node0, no -bind, expected to bind on any + tor target.
+        self.expected.append(
+            [
+                [],
+                [(any_ipv4, p2p_port(0)), (loopback_ipv4, REGTEST_TOR_TARGET_PORT)]
+            ]
+        )
+
+        # Node1, no -bind=...=onion, thus no extra port for Tor target.
+        self.expected.append(
+            [
+                ['-bind=127.0.0.1:{}'.format(port)],
+                [(loopback_ipv4, port)]
+            ],
+        )
+        port += 1
+
+        # Node2, no normal -bind, thus only the Tor target.
+        self.expected.append(
+            [
+                ['-bind=127.0.0.1:{}=onion'.format(port)],
+                [(loopback_ipv4, port)]
+            ],
+        )
+        port += 1
+
+        # Node3, both -bind and -bind=...=onion.
+        self.expected.append(
+            [
+                ['-bind=127.0.0.1:{}'.format(port), '-bind=127.0.0.1:{}=onion'.format(port + 1)],
+                [(loopback_ipv4, port), (loopback_ipv4, port + 1)]
+            ],
+        )
+        port += 2
+
+        # Add RPC ports to the list of expected ports to bind to for all nodes.
+        # They are not relevant for this test.
+        for i in range(len(self.expected)):
+            self.expected[i][1].append((loopback_ipv4, rpc_port(i)))
+
+        self.extra_args = list(map(lambda e: e[0], self.expected))
+        self.setup_nodes()
+
+    def run_test(self):
+        for i in range(len(self.expected)):
+            pid = self.nodes[i].process.pid
+            actual = set(get_bind_addrs(pid))
+            # Remove IPv6 addresses because on some CI environments "::1" is not configured
+            # on the system (so our test_ipv6_local() would return False), but it is
+            # possible to bind on "::". This makes it unpredictable whether to expect
+            # that bitcoind has bound on "::1" (for RPC) and "::" (for P2P).
+            actual_without_ipv6 = set(filter(lambda e: len(e[0]) != 32, actual))
+            assert_equal(actual_without_ipv6, set(self.expected[i][1]))
+
+if __name__ == '__main__':
+    BindExtraTest().main()
